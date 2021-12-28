34ff81e01e8b02d080bdf6f917abb0c61415764c
BrightBulb123 <83759628+BrightBulb123@users.noreply.github.com>
2021-12-28 12:42:15 +1100

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+"""
+This is a program to guide you through the game "Keep Talking and Nobody Explodes" by Steel Crate Games.
+"""
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
